fc649fa1098f1434b0b2c31edc8f2178e78bbf8c
labs34 <131676255+labs34@users.noreply.github.com>
2023-05-08 00:32:43 +0530

    Update first.R

--- first.R
+++ first.R
@@ -1,32 +1,30 @@
 n <- as.integer(readline(prompt = "Enter no of students"))
 
-USN <- vector(mode="character", length= n) 
-Name <- vector(mode="character", length= n)
-Marks <- vector(mode="numeric", length= n)
+usn <- vector(mode="character", length= n) 
+name <- vector(mode="character", length= n)
+marks <- vector(mode="character", length= n)
 
-print("Enter USN")
-for (i in 1:n)
-	USN[i] <- as.character(readline())
-print("Enter Name")
-for (i in 1:n)
-	Name [i] <- readline()
-print("Enter Marks" )
-for (i in 1:n)
-	Marks[i] <- as.integer(readline())
+print("Enter usn,name ,marks")
+for (i in 1:n){
+	usn[i] <- as.character(readline())
+	name [i] <-as.character(readline())
+	marks[i] <- as.integer(readline())
+	}
 	
-	
-student <- data.frame(USN,Name,Marks)
+student <- data.frame(usn,name,marks)
 print("The Student detials are as follows") 
 print(student)
 
 print("Enter Age")
-Age <- vector(mode="integer", length=n)
-for (i in 1:n)
-	Age [i] <- readline()
-student <- cbind(student,Age)
+age <- vector(mode="numeric", length=n)
+for (i in 1:n){
+	age [i] <- as.integer(readline())
+}
+student <- cbind(student,age)
 print(student)
 
-for(i in 1:n)
+for(i in 1:n){
 	if ( student[i,][3] > 25 )
-		if (student[i,][4] > 20)
+		if (student[i,][4] < 20)
 			print(student[i,])
+}
